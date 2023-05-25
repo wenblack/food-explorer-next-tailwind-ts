daf58533999a54191f1613e13aca874f43df438e
Wender Barbosa <wenblack012@gmail.com>
2023-05-25 16:53:53 -0300

    ⚙️ Remove default colors and import news for testing ⚙️

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        bgheader: '#00111A',
+        bgfooter: '#00111A',
+        bgprimaray: '#000A0F'
       },
     },
   },
