baec5e956c283e90574cb0d158b0c2bf3214be9d
Wender Barbosa <wenblack012@gmail.com>
2023-05-29 12:53:21 -0300

    📱 Create final header layout(mobile/responsive) ��

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -14,7 +14,7 @@ const poppins = Poppins({ subsets: ['latin'], weight: ['500', '400', '700', '600
 export function Button({number, order, value}:ButtonProps){
     if(order){
         return(
-            <button className={`flex h-12 w-[215px] items-center  bg-bgbutton gap-3 rounded-lg justify-center py-[14px] bg-redButton ${poppins.className}`}>
+            <button className={`flex hover:opacity-80 h-12 w-[215px] items-center  bg-bgbutton gap-3 rounded-lg justify-center py-[14px] bg-redButton ${poppins.className}`}>
                 <Image src={orderIcon} alt="App polygon logo" width={30} height={30}></Image>
                 <span>
                     {value} ({number})
@@ -23,7 +23,7 @@ export function Button({number, order, value}:ButtonProps){
         )
     }
     return(
-            <button className={`flex h-12 w-full items-center bg-bgbutton gap-3 rounded-lg justify-center py-[14px] bg-redButton ${poppins.className}`}>
+            <button className={`flex h-12 hover:opacity-80 w-full items-center bg-bgbutton gap-3 rounded-lg justify-center py-[14px] bg-redButton ${poppins.className}`}>
                 <span>
                     {value}
                 </span>
--- src/components/MenuMobile/index.tsx
+++ src/components/MenuMobile/index.tsx
@@ -45,22 +45,21 @@ export default function Example() {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                    <span className={`flex gap-3 items-center font-bold text-xl text-title lg:hidden ${poppins.className}`}>
-                        <Image src={logo} alt="App polygon logo" width={25} height={25}></Image>
-                        food explorer
+                    <span className={`flex gap-3 items-center font-bold text-xl text-title sm:hidden md:hidden lg:hidden ${poppins.className}`}>
+                        <form action="" className="flex text-sm h-full w-full  max-w-[400px] bg-bgForm gap-3 rounded-lg px-[14px] py-[12px]">
+                      <Image src={search} alt="Search logo Icon" width={30} height={30}></Image>
+                      <input className="bg-transparent w-full outline-none font-normal text-base " placeholder="Busque pelas opções"></input>
+                    </form>
                     </span>
                      <span className={` hidden md:hidden sm:hidden gap-3 items-center font-bold text-2xl text-title lg:flex ${poppins.className}`}>
                         <Image src={logo} alt="App polygon logo" width={30} height={30}></Image>
                         food explorer
                     </span>
                 </div>
-                <div className="hidden w-full sm:ml-6 sm:block">
+                <div className="hidden w-full  sm:ml-2 sm:block">
                   <div className="flex h-full items-center gap-8">
-                    <Link className="flex  md:flex lg:flex sm:hidden items-center ml-8 font-normal text-base" href={'#'}>
+                    <Link className="flex  md:flex lg:flex  items-center ml-8 font-normal text-base" href={'#'}>
                       Meus Favoritos
-                    </Link>
-                    <Link className="hidden md:hidden sm:flex  items-center ml-8 font-normal text-sm" href={'#'}>
-                      <Image className='hover:opacity-70' src={favoriteIcon} alt="App polygon logo" width={25} height={25}></Image>
                     </Link>
                     <form action="" className="flex sm:hidden md:flex h-full w-full min-w-[250px] max-w-[410px] bg-bgForm gap-3 rounded-lg px-[16px] py-[14px]">
                       <Image src={search} alt="Search logo Icon" ></Image>
@@ -70,11 +69,11 @@ export default function Example() {
                       <Image src={search} alt="Search logo Icon" width={30} height={30}></Image>
                       <input className="bg-transparent w-full outline-none font-normal text-base " placeholder="Busque pelas opções"></input>
                     </form>
-                    <Link className="hidden md:hidden sm:flex  items-center font-normal text-sm" href={'#'}>
-                      <Image className='hover:opacity-70' src={favoriteIcon} alt="App polygon logo"  ></Image>
-                    </Link>
                     <span className="flex sm:hidden md:flex">
                     <Button value="Meu pedido " order  number={0}/>
+                    </span>
+                    <span className="flex w-[120px] md:hidden lg:hidden">
+                    <Button value="" order  number={0}/>
                     </span>
                   </div>
                   
@@ -83,7 +82,7 @@ export default function Example() {
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto  sm:pr-0">
                 <Link href={'#'} className="ml-8 hover:opacity-70" >
                   <Image src={logOutIcon} alt="LogOut Icon"></Image>
-               </Link> 
+               </Link>
               </div>
             </div>
           </div>
