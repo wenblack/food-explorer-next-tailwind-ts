4b9b6106bcf8b2978b66c22d42082f2337da589a
Wender Barbosa <wenblack012@gmail.com>
2023-05-31 12:09:56 -0300

    ➕ Create Login Page ➕

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,13 +3,15 @@ interface inputProps{
     label: string
 }
 
+const id = String(Math.floor(Math.random() * 100))
+
 export function Input ({value, label}:inputProps){
     return(
-        <>  
+        <div className="flex flex-col gap-2 ">  
             <label htmlFor="form">{label}</label>
-            <form id="form" name="form" action="" className="flex font-normal w-[70vw]  max-w-[348px] h-12 bg-bgForm gap-3 rounded-lg pl-[14px]">
-                <input autoFocus autoComplete="email" className="bg-transparent w-full outline-none font-normal text-base " placeholder={value}></input>
+            <form id={id} name={id}  className="flex  focus-within:ring-1 ring-white font-normal w-[80vw]  max-w-[348px] h-12 bg-bgForm gap-3 rounded-lg pl-[14px]">
+                <input className="bg-transparent  w-full outline-none font-normal text-base " placeholder={value}></input>
             </form>            
-        </>
+        </div >
     )
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,26 +1,44 @@
 import { Logo } from '@/components/Logo'
-import { Roboto_Flex, Poppins } from 'next/font/google'
+import { Roboto_Flex } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import { Input } from '@/components/Input'
 import { Heading } from '@/components/Heading'
 import { Button } from '@/components/Button'
+import Link from 'next/link'
 
 const roboto = Roboto_Flex({ subsets: ['latin'] })
-const poppins = Poppins({ subsets: ['latin'] , weight:['700','600']})
+const poppins = Poppins({ subsets: ['latin'] , weight:['700','600','500']})
 
 export default function Login() {
   return (
-      <main className={`min-h-screen overflow-hidden h-fulll grid md:grid-cols-2 lg:grid-cols-2 sm:grid-cols-1 w-screen bg-bgPrimary  ${roboto.className}`}>
-       <div className='hidden  bg-bgPrimary text-white md:grid  lg:grid md:h-[100vh] items-center justify-center'> 
+      <main className={`min-h-screen sm:overflow-auto md:flex-col md:flex md:justify-evenly lg:overflow-hidden lg:grid  h-fulll grid md:grid-cols-2 lg:grid-cols-2  w-screen bg-bgPrimary  ${roboto.className}`}>
+       {/* Logo Column(Desktop)*/}
+       <div className='hidden md:flex bg-bgPrimary text-white   lg:grid md:h-full items-center justify-center'> 
         <span className={`flex items-center gap-3 text-[42px] font-bold ${poppins.className}`}>
         <Logo></Logo>
         </span>
        </div>
-       <div className=' text-white grid h-[100vh] items-center justify-center'>
-        <div className=' flex flex-col items-center justify-center h-[100vh] w-[80vw] max-h-[540px] max-w-[476px] bg-bgContentForm rounded-lg'>
+        {/* Form Column */}
+       <div className=' text-white md:h-full  grid h-[100vh] items-center justify-center bg-bgPrimary'>
+         {/* Desktop Version*/}
+        <div className=' hidden md:flex lg:flex flex-col  items-center justify-center h-[100vh] w-[100vw] max-h-[540px] md:min-h-[540px] max-w-[476px] md:m-8 bg-bgContentForm rounded-lg'>
           <Heading h1 value='Faça login' ></Heading>
-          <Input value='Exemplo: exemplo@exemplo.com.br' label='Email' />
+          <div className='w-[348px] mt-8 flex flex-col gap-8 '>
+             <Input value='Exemplo: exemplo@exemplo.com.br' label='Email' />
           <Input value='No mínimo 6 caracteres' label='Senha' />
-          <Button value='Entrar' />
+          <Button value='Entrar' />    
+          </div>
+          <Link className={`${poppins.className} hover:opacity-70  font-medium mt-8`} href={'#'}>Criar uma conta</Link>
+        </div>
+        {/* Mobile version*/}
+        <div className=' flex md:hidden sm:flex  lg:hidden flex-col  items-center justify-center h-[100vh] w-[100vw] sm:min-h-[540px] md:max-w-[476px]  bg-bgContentForm rounded-lg'>
+          <Heading h1 value='Faça login' ></Heading>
+          <div className='w-[348px] justify-center items-center mt-8 flex flex-col gap-8 '>
+            <Input value='Exemplo: exemplo@exemplo.com.br' label='Email' />
+            <Input value='No mínimo 6 caracteres' label='Senha' />
+            <Button value='Entrar' />            
+          </div>
+          <Link className={`${poppins.className} hover:opacity-70  font-medium mt-8`} href={'#'}>Criar uma conta</Link>
         </div>
        </div>
       </main>
